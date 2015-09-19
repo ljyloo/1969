8bac39b264f3b26290a2fb4335cecb459cf0cd16
Seahub <18933932386@163.com>
2015-09-19 17:13:33 +0800

    OldVersion Upload

---
+++ 1969 OldVersion/Thousands/Classes/Controllers/ShowsTableViewController/ShowsCell/ShowsTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ShowsTableViewCell.h
+//  Thousands
+//
+//  Created by SeaHub on 15/9/9.
+//  Copyright (c) 2015年 S3Develop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShowsTableViewCell : UITableViewCell
+
+@end
