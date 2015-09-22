8bf347603c311d1e321ea892719a758bbc8bf532
Seahub <18933932386@163.com>
2015-09-22 10:21:46 +0800

    add LongPressSaveImageFeature

---
+++ 1969 Feature/Thousands/Supporting Files/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Thousands
+//
+//  Created by SeaHub on 15/9/9.
+//  Copyright (c) 2015年 S3Develop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
